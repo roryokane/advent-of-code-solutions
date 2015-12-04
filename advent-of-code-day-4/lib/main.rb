402ec693244e0ae61be0ffd0cc000ec5c34a05c7
Rory O’Kane <rory@roryokane.com>
2015-12-04 04:16:11 -0500

    Complete day 4 part 1

--- advent-of-code-day-4/lib/main.rb
+++ advent-of-code-day-4/lib/main.rb
@@ -1,9 +1,35 @@
-class Person
-  def greet
-    "Hello!"
+require 'openssl'
+
+class Hasher
+  class << self
+    def md5(secret_key, number)
+      digest = OpenSSL::Digest::MD5.new
+      return digest.hexdigest(secret_key + number.to_s)
+    end
+    
+    def lowest_positive_integer_creating_hash_satisfying(secret_key, &predicate)
+      num = 1
+      hash_str = md5(secret_key, num)
+      until predicate.call(hash_str)
+        num += 1
+        hash_str = md5(secret_key, num)
+      end
+      return num
+    end
+    
+    def starts_with_zeroes_proc(num_zeroes)
+      zeroes_string = "0" * num_zeroes
+      return Proc.new { |str| str.start_with?(zeroes_string) }
+    end
+    
+    def lowest_initial_zeroes_added_number(secret_key, num_zeroes)
+      lowest_positive_integer_creating_hash_satisfying(secret_key, &starts_with_zeroes_proc(num_zeroes))
+    end
   end
 end
 
 
 if __FILE__ == $0
+  secret_key = gets.chomp
+  puts Hasher.lowest_initial_zeroes_added_number(secret_key, 5)
 end
--- advent-of-code-day-4/test/main_test.rb
+++ advent-of-code-day-4/test/main_test.rb
@@ -1,8 +1,18 @@
 require_relative 'test_helper'
 
 class MainTest < MiniTest::Test
-  def test_hello
-    person = Person.new
-    assert_equal "Hello!", person.greet
+  def test_md5
+    assert_equal "000001dbbfa3a5c83a2d506429c7b00e", Hasher.md5("abcdef", 609043)
+  end
+  
+  def test_starts_with_zeroes_proc
+    three_zeroes_prefix = Hasher.starts_with_zeroes_proc(3)
+    assert_equal true, three_zeroes_prefix.call("000abc")
+    assert_equal false, three_zeroes_prefix.call("00def")
+    assert_equal false, three_zeroes_prefix.call("ghi000")
+  end
+  
+  def test_lowest_initial_zeroes_added_number
+    assert_equal 609043, Hasher.lowest_initial_zeroes_added_number("abcdef", 5)
   end
 end

d4612ede861e3cebe6982c5e3544143284643354
Rory O’Kane <rory@roryokane.com>
2015-12-05 14:44:03 -0500

    Complete day 5 part 2

--- advent-of-code-day-5/src/advent_of_code_day_5/core.clj
+++ advent-of-code-day-5/src/advent_of_code_day_5/core.clj
@@ -1,29 +1,42 @@
 (ns advent-of-code-day-5.core
   (:gen-class))
 
-(defn- contains-three-vowels [string]
+
+(defn- contains-three-vowels? [string]
   (>= (count (re-seq #"[aeiou]" string)) 3))
 
-(defn- contains-double-letter [string]
+(defn- contains-double-letter? [string]
   (re-find #"(.)\1" string))
 
 (def naughty-strings ["ab" "cd" "pq" "xy"])
 
-(defn- does-not-contain-naughty-string [string]
+(defn- does-not-contain-naughty-string? [string]
   (not-any? (fn [naughty-string]
               (.contains string naughty-string))
             naughty-strings))
 
-(defn nice? [string]
-  (boolean (and (contains-three-vowels string)
-                (contains-double-letter string)
-                (does-not-contain-naughty-string string))))
+(defn old-nice? [string]
+  (boolean (and (contains-three-vowels? string)
+                (contains-double-letter? string)
+                (does-not-contain-naughty-string? string))))
+
+
+(defn- contains-repeated-pair? [string]
+  (re-find #"(..).*\1" string))
+
+(defn- contains-repeated-letter-with-one-char-gap? [string]
+  (re-find #"(.).\1" string))
+
+(defn new-nice? [string]
+  (boolean (and (contains-repeated-pair? string)
+                (contains-repeated-letter-with-one-char-gap? string))))
+
 
 (defn -main
   "Print the count of nice strings in standard input."
   [& args]
   (->> (slurp *in*)
        (clojure.string/split-lines)
-       (filter nice?)
+       #_(filter old-nice?) (filter new-nice?)
        (count)
        (println)))
--- advent-of-code-day-5/test/advent_of_code_day_5/core_test.clj
+++ advent-of-code-day-5/test/advent_of_code_day_5/core_test.clj
@@ -3,9 +3,14 @@
             [advent-of-code-day-5.core :refer :all]))
 
 (deftest niceness-test
-  (testing "strings are correctly recognized as naughty or nice"
-    (is (= true (nice? "ugknbfddgicrmopn")))
-    (is (= true (nice? "aaa")))
-    (is (= false (nice? "jchzalrnumimnmhp")))
-    (is (= false (nice? "haegwjzuvuyypxyu")))
-    (is (= false (nice? "dvszwmarrgswjxmb")))))
+  (testing "strings are correctly recognized as naughty or nice with the old rules"
+    (is (= true (old-nice? "ugknbfddgicrmopn")))
+    (is (= true (old-nice? "aaa")))
+    (is (= false (old-nice? "jchzalrnumimnmhp")))
+    (is (= false (old-nice? "haegwjzuvuyypxyu")))
+    (is (= false (old-nice? "dvszwmarrgswjxmb"))))
+  (testing "strings are correctly recognized as naughty or nice with the new rules"
+    (is (= true (new-nice? "qjhvhtzxzqqjkmpb")))
+    (is (= true (new-nice? "xxyxx")))
+    (is (= false (new-nice? "uurcxstgmygtbstg")))
+    (is (= false (new-nice? "ieodomkazucvgmuy")))))

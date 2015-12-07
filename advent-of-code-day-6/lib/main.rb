efe3b40eed249301dd4b220edd614cf994d8f6a3
Rory O’Kane <rory@roryokane.com>
2015-12-07 16:50:19 -0500

    Complete day 6 part 1

--- advent-of-code-day-6/lib/main.rb
+++ advent-of-code-day-6/lib/main.rb
@@ -1,9 +1,111 @@
-class Person
-  def greet
-    "Hello!"
+class LightGrid
+  def initialize(side_length)
+    @grid = (0...side_length).map { (0...side_length).map { false } }
+  end
+  
+  def count_of_lit_lights
+    @grid.reduce(0) do |count, row|
+      count + row.count { |light_on| light_on }
+    end
+  end
+  
+  def turn_on!(start_coord, end_coord)
+    change_range!(start_coord, end_coord) { true }
+  end
+  
+  def turn_off!(start_coord, end_coord)
+    change_range!(start_coord, end_coord) { false }
+  end
+  
+  def toggle!(start_coord, end_coord)
+    change_range!(start_coord, end_coord) { |light_on| !light_on }
+  end
+  
+  private
+  
+  def change_range!(start_coord, end_coord)
+    start_x, start_y = start_coord
+    end_x, end_y = end_coord
+    (start_x..end_x).each do |x|
+      (start_y..end_y).each do |y|
+        @grid[y][x] = yield @grid[y][x] 
+      end
+    end
+  end
+end
+
+class InstructionsParser
+  def initialize(instructions_reader)
+    @instructions_reader = instructions_reader
+  end
+  
+  include Enumerable
+  def each
+    while line = @instructions_reader.gets
+      line = line.chomp
+      yield parse_one_instruction(line)
+    end
+  end
+  
+  private
+  
+  INSTRUCTION_REGEX = /^(.+) (\d+,\d+) through (\d+,\d+)$/
+  
+  def parse_one_instruction(instruction)
+    begin
+      matches = INSTRUCTION_REGEX.match(instruction)
+      return [
+        parse_instruction_name(matches[1]),
+        parse_coordinates(matches[2]),
+        parse_coordinates(matches[3]),
+      ]
+    rescue
+      raise ParseError.new, "improperly formatted instruction: ‘#{instruction}’"
+    end
+  end
+  
+  def parse_instruction_name(instruction_name)
+    case instruction_name
+    when "turn on" then :turn_on
+    when "turn off" then :turn_off
+    when "toggle" then :toggle
+    else raise
+    end
+  end
+  
+  def parse_coordinates(coordinates)
+    coordinates.split(',').map(&:to_i)
+  end
+
+  class ParseError < StandardError
+  end
+end
+
+class InstructionsExecutor
+  def initialize(light_grid)
+    @light_grid = light_grid
+  end
+  
+  def execute!(parsed_instructions)
+    parsed_instructions.each do |name, start_coord, end_coord|
+      method_name = grid_method_name_for_instruction(name)
+      @light_grid.send(method_name, start_coord, end_coord)
+    end
+    return nil
+  end
+  
+  private
+  
+  def grid_method_name_for_instruction(name)
+    (name.to_s + "!").to_sym
   end
 end
 
 
 if __FILE__ == $0
+  light_grid = LightGrid.new(1000)
+  parsed_instructions = InstructionsParser.new(STDIN)
+  executor = InstructionsExecutor.new(light_grid)
+  executor.execute!(parsed_instructions)
+  puts light_grid.count_of_lit_lights
 end
--- advent-of-code-day-6/test/main_test.rb
+++ advent-of-code-day-6/test/main_test.rb
@@ -1,8 +1,135 @@
+require 'stringio'
 require_relative 'test_helper'
 
-class MainTest < MiniTest::Test
-  def test_hello
-    person = Person.new
-    assert_equal "Hello!", person.greet
+class GridTest < MiniTest::Test
+  def test_empty_grid
+    grid = LightGrid.new(20)
+    assert_equal 0, grid.count_of_lit_lights
+  end
+  
+  def test_turn_on_one_light
+    grid = LightGrid.new(20)
+    grid.turn_on!([2,2], [2,2])
+    assert_equal 1, grid.count_of_lit_lights
+  end
+  
+  def test_turn_on_one_row
+    grid = LightGrid.new(20)
+    grid.turn_on!([2,2], [5,2])
+    assert_equal 4, grid.count_of_lit_lights
+  end
+  
+  def test_turn_on_one_column
+    grid = LightGrid.new(20)
+    grid.turn_on!([2,2], [2,5])
+    assert_equal 4, grid.count_of_lit_lights
+  end
+  
+  def test_turn_on_range
+    grid = LightGrid.new(20)
+    grid.turn_on!([2,2], [4,5])
+    assert_equal 12, grid.count_of_lit_lights
+  end
+  
+  def test_turn_on_full_range
+    grid = LightGrid.new(20)
+    grid.turn_on!([0,0], [19,19])
+    assert_equal 400, grid.count_of_lit_lights
+  end
+  
+  def test_turn_off_all
+    grid = LightGrid.new(20)
+    grid.turn_on!([0,0], [2,2])
+    grid.turn_off!([0,0], [19,19])
+    assert_equal 0, grid.count_of_lit_lights
+  end
+  
+  def test_turn_off_subset
+    grid = LightGrid.new(20)
+    grid.turn_on!([0,0], [0,2])
+    grid.turn_off!([0,1], [0,2])
+    assert_equal 1, grid.count_of_lit_lights
+  end
+  
+  def test_toggle
+    grid = LightGrid.new(20)
+    grid.turn_on!([0,0], [4,0])
+    grid.toggle!([0,0], [6,0])
+    assert_equal 2, grid.count_of_lit_lights
+  end
+  
+  def test_toggle_cancels_itself
+    grid = LightGrid.new(20)
+    grid.toggle!([0,0], [2,2])
+    grid.toggle!([0,0], [2,2])
+    assert_equal 0, grid.count_of_lit_lights
+  end
+end
+
+class InstructionParsingTest < MiniTest::Test
+  def test_parse_no_instructions
+    instructions = ""
+    parsed = InstructionsParser.new(make_reader(instructions))
+    assert_equal [], parsed.to_a
+  end
+  
+  def test_parse_one_instruction
+    instructions = "turn on 0,0 through 999,999"
+    parsed = InstructionsParser.new(make_reader(instructions))
+    assert_equal [[:turn_on, [0,0], [999,999]]], parsed.to_a
+  end
+  
+  def test_parse_one_instruction_with_ending_newline
+    instructions = "turn on 0,0 through 999,999\n"
+    parsed = InstructionsParser.new(make_reader(instructions))
+    assert_equal [[:turn_on, [0,0], [999,999]]], parsed.to_a
+  end
+  
+  def test_parse_multiple_instructions
+    instructions = [
+      "turn on 0,0 through 999,999",
+      "toggle 0,0 through 999,0",
+      "turn off 499,499 through 500,500",
+    ].join("\n")
+    parsed = InstructionsParser.new(make_reader(instructions))
+    assert_equal [
+      [:turn_on, [0,0], [999,999]],
+      [:toggle, [0,0], [999,0]],
+      [:turn_off, [499,499], [500,500]],
+    ], parsed.to_a
+  end
+  
+  def test_raise_when_parsing_unknown_instruction
+    bad_instruction = "color lights 0,0 through 999,999 red"
+    instructions = bad_instruction
+    parsed = InstructionsParser.new(make_reader(instructions))
+    
+    error = assert_raises(InstructionsParser::ParseError) { parsed.to_a }
+    assert_includes error.message, bad_instruction
+  end
+  
+  private
+  
+  def make_reader(instruction_string)
+    StringIO.new(instruction_string)
+  end
+end
+
+class InstructionExecutingTest < MiniTest::Test
+  def test_instruction_execution
+    parsed_instructions = [
+      [:turn_on, [0,0], [999,999]],
+      [:toggle, [0,0], [999,0]],
+      [:turn_off, [499,499], [500,500]],
+    ]
+    light_grid = Minitest::Mock.new
+    light_grid.expect :turn_on!, nil, [[0,0], [999,999]]
+    light_grid.expect :toggle!, nil, [[0,0], [999,0]]
+    light_grid.expect :turn_off!, nil, [[499,499], [500,500]]
+    
+    executor = InstructionsExecutor.new(light_grid)
+    executor.execute!(parsed_instructions)
+    
+    light_grid.verify
   end
 end

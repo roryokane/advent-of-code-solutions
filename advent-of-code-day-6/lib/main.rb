27020d77af415435f297aa84d568f1181f9c2c01
Rory O’Kane <rory@roryokane.com>
2015-12-08 00:29:16 -0500

    Complete day 6 part 2

--- advent-of-code-day-6/lib/main.rb
+++ advent-of-code-day-6/lib/main.rb
@@ -1,38 +1,78 @@
-class LightGrid
+class OnOffLightGrid
   def initialize(side_length)
-    @grid = (0...side_length).map { (0...side_length).map { false } }
+    @grid = RectangularRangeChangableGrid.new(side_length, false)
   end
   
   def count_of_lit_lights
-    @grid.reduce(0) do |count, row|
+    @grid.rows.reduce(0) do |count, row|
       count + row.count { |light_on| light_on }
     end
   end
   
   def turn_on!(start_coord, end_coord)
-    change_range!(start_coord, end_coord) { true }
+    @grid.change_range!(start_coord, end_coord) { true }
   end
   
   def turn_off!(start_coord, end_coord)
-    change_range!(start_coord, end_coord) { false }
+    @grid.change_range!(start_coord, end_coord) { false }
   end
   
   def toggle!(start_coord, end_coord)
-    change_range!(start_coord, end_coord) { |light_on| !light_on }
+    @grid.change_range!(start_coord, end_coord) { |light_on| !light_on }
+  end
+end
+
+class BrightnessLightGrid
+  def initialize(side_length)
+    @grid = RectangularRangeChangableGrid.new(side_length, 0)
+  end
+  
+  def total_brightness
+    @grid.rows.reduce(0) do |sum, row|
+      sum + row.reduce(0) do |row_sum, brightness|
+        row_sum + brightness
+      end
+    end
+  end
+  
+  def increase_brightness!(start_coord, end_coord, by_amount)
+    @grid.change_range!(start_coord, end_coord) do |brightness|
+      brightness + by_amount
+    end
+  end
+  
+  def decrease_brightness!(start_coord, end_coord, by_amount)
+    @grid.change_range!(start_coord, end_coord) do |brightness|
+      [brightness - by_amount, MIN_BRIGHTNESS].max
+    end
   end
   
   private
   
+  MIN_BRIGHTNESS = 0
+end
+
+class RectangularRangeChangableGrid
+  def initialize(side_length, default_value)
+    @grid = (0...side_length).map { (0...side_length).map { default_value } }
+  end
+  
+  # Rows is an Array of rows. Each row is an Array of values.
+  def rows
+    @grid
+  end
+  
   def change_range!(start_coord, end_coord)
-    start_x, start_y = start_coord
-    end_x, end_y = end_coord
-    (start_x..end_x).each do |x|
-      (start_y..end_y).each do |y|
-        @grid[y][x] = yield @grid[y][x] 
+    start_col, start_row = start_coord
+    end_col, end_row = end_coord
+    (start_col..end_col).each do |col|
+      (start_row..end_row).each do |row|
+        rows[row][col] = yield rows[row][col] 
       end
     end
   end
 end
+
 
 class InstructionsParser
   def initialize(instructions_reader)
@@ -81,7 +121,8 @@ class InstructionsParser
   end
 end
 
-class InstructionsExecutor
+
+class OnOffInstructionsExecutor
   def initialize(light_grid)
     @light_grid = light_grid
   end
@@ -101,11 +142,36 @@ class InstructionsExecutor
   end
 end
 
+class BrightnessInstructionsExecutor
+  def initialize(light_grid)
+    @light_grid = light_grid
+  end
+  
+  def execute!(parsed_instructions)
+    parsed_instructions.each do |name, start_coord, end_coord|
+      case name
+      when :turn_on
+        @light_grid.increase_brightness! start_coord, end_coord, 1
+      when :toggle
+        @light_grid.increase_brightness! start_coord, end_coord, 2
+      when :turn_off
+        @light_grid.decrease_brightness! start_coord, end_coord, 1
+      end
+    end
+    return nil
+  end
+end
+
 
 if __FILE__ == $0
-  light_grid = LightGrid.new(1000)
+  VERSION_TO_RUN = 1 # put 0 for part 1, 1 for part 2
+  light_grid_type = [OnOffLightGrid, BrightnessLightGrid][VERSION_TO_RUN]
+  executor_type = [OnOffInstructionsExecutor, BrightnessInstructionsExecutor][VERSION_TO_RUN]
+  result_method_name = [:count_of_lit_lights, :total_brightness][VERSION_TO_RUN]
+  
+  light_grid = light_grid_type.new(1000)
   parsed_instructions = InstructionsParser.new(STDIN)
-  executor = InstructionsExecutor.new(light_grid)
+  executor = executor_type.new(light_grid)
   executor.execute!(parsed_instructions)
-  puts light_grid.count_of_lit_lights
+  puts light_grid.send(result_method_name)
 end
--- advent-of-code-day-6/test/main_test.rb
+++ advent-of-code-day-6/test/main_test.rb
@@ -1,70 +1,105 @@
 require 'stringio'
 require_relative 'test_helper'
 
-class GridTest < MiniTest::Test
+class OnOffGridTest < MiniTest::Test
   def test_empty_grid
-    grid = LightGrid.new(20)
+    grid = OnOffLightGrid.new(20)
     assert_equal 0, grid.count_of_lit_lights
   end
   
   def test_turn_on_one_light
-    grid = LightGrid.new(20)
+    grid = OnOffLightGrid.new(20)
     grid.turn_on!([2,2], [2,2])
     assert_equal 1, grid.count_of_lit_lights
   end
   
   def test_turn_on_one_row
-    grid = LightGrid.new(20)
+    grid = OnOffLightGrid.new(20)
     grid.turn_on!([2,2], [5,2])
     assert_equal 4, grid.count_of_lit_lights
   end
   
   def test_turn_on_one_column
-    grid = LightGrid.new(20)
+    grid = OnOffLightGrid.new(20)
     grid.turn_on!([2,2], [2,5])
     assert_equal 4, grid.count_of_lit_lights
   end
   
   def test_turn_on_range
-    grid = LightGrid.new(20)
+    grid = OnOffLightGrid.new(20)
     grid.turn_on!([2,2], [4,5])
     assert_equal 12, grid.count_of_lit_lights
   end
   
   def test_turn_on_full_range
-    grid = LightGrid.new(20)
+    grid = OnOffLightGrid.new(20)
     grid.turn_on!([0,0], [19,19])
     assert_equal 400, grid.count_of_lit_lights
   end
   
   def test_turn_off_all
-    grid = LightGrid.new(20)
+    grid = OnOffLightGrid.new(20)
     grid.turn_on!([0,0], [2,2])
     grid.turn_off!([0,0], [19,19])
     assert_equal 0, grid.count_of_lit_lights
   end
   
   def test_turn_off_subset
-    grid = LightGrid.new(20)
+    grid = OnOffLightGrid.new(20)
     grid.turn_on!([0,0], [0,2])
     grid.turn_off!([0,1], [0,2])
     assert_equal 1, grid.count_of_lit_lights
   end
   
   def test_toggle
-    grid = LightGrid.new(20)
+    grid = OnOffLightGrid.new(20)
     grid.turn_on!([0,0], [4,0])
     grid.toggle!([0,0], [6,0])
     assert_equal 2, grid.count_of_lit_lights
   end
   
   def test_toggle_cancels_itself
-    grid = LightGrid.new(20)
+    grid = OnOffLightGrid.new(20)
     grid.toggle!([0,0], [2,2])
     grid.toggle!([0,0], [2,2])
     assert_equal 0, grid.count_of_lit_lights
   end
 end
+
+class BrightnessGridTest < MiniTest::Test
+  def test_empty_grid
+    grid = BrightnessLightGrid.new(20)
+    assert_equal 0, grid.total_brightness
+  end
+  
+  def test_increase_brightness_of_one_light
+    grid = BrightnessLightGrid.new(20)
+    grid.increase_brightness!([2,2], [2,2], 10)
+    assert_equal 10, grid.total_brightness
+  end
+  
+  def test_increase_brightness_of_range
+    grid = BrightnessLightGrid.new(20)
+    grid.increase_brightness!([2,2], [4,5], 1)
+    assert_equal 12, grid.total_brightness
+  end
+  
+  def test_decrease_brightness
+    grid = BrightnessLightGrid.new(20)
+    grid.increase_brightness!([2,2], [4,5], 3)
+    grid.decrease_brightness!([2,2], [4,5], 1)
+    assert_equal 24, grid.total_brightness
+  end
+  
+  def test_decrease_brightness_has_minimum_of_0
+    grid = BrightnessLightGrid.new(20)
+    grid.increase_brightness!([2,2], [2,2], 5)
+    grid.decrease_brightness!([2,2], [2,2], 10)
+    grid.increase_brightness!([2,2], [2,2], 1)
+    assert_equal 1, grid.total_brightness
+  end
+end
+
 
 class InstructionParsingTest < MiniTest::Test
   def test_parse_no_instructions
@@ -115,8 +150,9 @@ class InstructionParsingTest < MiniTest::Test
   end
 end
 
+
 class InstructionExecutingTest < MiniTest::Test
-  def test_instruction_execution
+  def test_on_off_instruction_execution
     parsed_instructions = [
       [:turn_on, [0,0], [999,999]],
       [:toggle, [0,0], [999,0]],
@@ -127,7 +163,24 @@ class InstructionExecutingTest < MiniTest::Test
     light_grid.expect :toggle!, nil, [[0,0], [999,0]]
     light_grid.expect :turn_off!, nil, [[499,499], [500,500]]
     
-    executor = InstructionsExecutor.new(light_grid)
+    executor = OnOffInstructionsExecutor.new(light_grid)
+    executor.execute!(parsed_instructions)
+    
+    light_grid.verify
+  end
+  
+  def test_brightness_instruction_execution
+    parsed_instructions = [
+      [:turn_on, [0,0], [999,999]],
+      [:toggle, [0,0], [999,0]],
+      [:turn_off, [499,499], [500,500]],
+    ]
+    light_grid = Minitest::Mock.new
+    light_grid.expect :increase_brightness!, nil, [[0,0], [999,999], 1]
+    light_grid.expect :increase_brightness!, nil, [[0,0], [999,0], 2]
+    light_grid.expect :decrease_brightness!, nil, [[499,499], [500,500], 1]
+    
+    executor = BrightnessInstructionsExecutor.new(light_grid)
     executor.execute!(parsed_instructions)
     
     light_grid.verify

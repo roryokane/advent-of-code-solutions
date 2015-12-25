a5a7dcfcdb33d646bcf81fc0b117006cc86b1b43
Rory O’Kane <rory@roryokane.com>
2015-12-25 02:35:43 -0500

    Complete day 12 part 1

--- advent-of-code-day-12/lib/main.rb
+++ advent-of-code-day-12/lib/main.rb
@@ -6,14 +6,25 @@ def parse_number_string(num_str)
   return array[0]
 end
 
+# converted from the `number` diagram on http://json.org/
+JSON_NUMBER_REGEX = /-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+\-]?\d+)?/
+
+def extract_number_strings(json_with_nums)
+  json_with_nums.scan(JSON_NUMBER_REGEX)
+end
+
 def extract_numbers(json_with_nums)
   extract_number_strings(json_with_nums).map do |num_str|
     parse_number_string(num_str)
   end
 end
 
+def sum_extracted_numbers(json_with_nums)
+  nums = extract_numbers(json_with_nums)
+  return nums.reduce(0, :+)
+end
+
 if __FILE__ == $0
   json_with_nums = $stdin.read
-  nums = extract_numbers(json_with_nums)
-  puts nums.reduce(0, :+)
+  puts sum_extracted_numbers(json_with_nums)
 end
--- advent-of-code-day-12/test/main_test.rb
+++ advent-of-code-day-12/test/main_test.rb
@@ -10,4 +10,19 @@ class MainTest < MiniTest::Test
     num_str = '-1.5e1'
     assert_equal (-15), parse_number_string(num_str)
   end
+  
+  def test_extract_simple_number_strings
+    json = '[1, "abc", {"def": 23}]'
+    assert_equal ['1', '23'], extract_number_strings(json)
+  end
+  
+  def test_extract_strings_for_numbers_with_lots_of_notation
+    json = '[-1.5e1, ["e", 3.66666]]'
+    assert_equal ['-1.5e1', '3.66666'], extract_number_strings(json)
+  end
+  
+  def test_sum_extracted_numbers
+    json = '[1, "abc", {"def": 23}]'
+    assert_equal 24, sum_extracted_numbers(json)
+  end
 end

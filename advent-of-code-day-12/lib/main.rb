8e77cf93a199a0fe4d24e7444f103fb6e6c4ac74
Rory O’Kane <rory@roryokane.com>
2015-12-25 02:24:34 -0500

    Implement `parse_number_string` for day 12 part 1

--- advent-of-code-day-12/lib/main.rb
+++ advent-of-code-day-12/lib/main.rb
@@ -1,9 +1,19 @@
-class Person
-  def greet
-    "Hello!"
+require 'json'
+
+def parse_number_string(num_str)
+  array_str = '[' + num_str + ']'
+  array = JSON.parse(array_str)
+  return array[0]
+end
+
+def extract_numbers(json_with_nums)
+  extract_number_strings(json_with_nums).map do |num_str|
+    parse_number_string(num_str)
   end
 end
 
-
 if __FILE__ == $0
+  json_with_nums = $stdin.read
+  nums = extract_numbers(json_with_nums)
+  puts nums.reduce(0, :+)
 end
--- advent-of-code-day-12/test/main_test.rb
+++ advent-of-code-day-12/test/main_test.rb
@@ -1,8 +1,13 @@
 require_relative 'test_helper'
 
 class MainTest < MiniTest::Test
-  def test_hello
-    person = Person.new
-    assert_equal "Hello!", person.greet
+  def test_parse_one_number
+    num_str = '135'
+    assert_equal 135, parse_number_string(num_str)
+  end
+  
+  def test_parse_number_with_lots_of_notation
+    num_str = '-1.5e1'
+    assert_equal (-15), parse_number_string(num_str)
   end
 end

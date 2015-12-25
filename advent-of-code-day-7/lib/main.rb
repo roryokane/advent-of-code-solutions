02bee73da97d1d8449efc536988ef55ea8e95930
Rory O’Kane <rory@roryokane.com>
2015-12-25 02:13:55 -0500

    Start implementing day 7, part 1 using Parslet

--- advent-of-code-day-7/lib/main.rb
+++ advent-of-code-day-7/lib/main.rb
@@ -1,6 +1,61 @@
-class Person
-  def greet
-    "Hello!"
+require 'parslet'
+
+class ExploratoryParser < Parslet::Parser
+  root(:instruction)
+  rule(:instruction) {
+    source.as(:source) >> str(' -> ') >> destination.as(:destination)
+  }
+  
+  rule(:source) {
+    literal.as(:literal) |
+    unary_op.as(:unary_op) |
+    binary_op.as(:binary_op)
+  }
+  rule(:destination) { wire_name }
+  
+  rule(:literal) { integer }
+  rule(:unary_op) { not_source.as(:not) | shift_op }
+  rule(:shift_op) { lshift_source.as(:lshift) | rshift_source.as(:rshift) }
+  rule(:binary_op) { and_source.as(:and) | or_source.as(:or) }
+  
+  rule(:not_source) {
+    str('NOT') >> sp >> wire_name.as(:wire)
+  }
+  rule(:lshift_source) {
+    wire_name.as(:wire) >> sp >> str('LSHIFT') >> sp >> integer.as(:shift_by)
+  }
+  rule(:rshift_source) {
+    wire_name.as(:wire) >> sp >> str('RSHIFT') >> sp >> integer.as(:shift_by)
+  }
+  rule(:and_source) {
+    wire_name.as(:wire1) >> sp >> str('AND') >> sp >> wire_name.as(:wire2)
+  }
+  rule(:or_source) {
+    wire_name.as(:wire1) >> sp >> str('OR') >> sp >> wire_name.as(:wire2)
+  }
+  
+  rule(:sp) { str(' ') } # space
+  rule(:integer) { match('[0-9]').repeat(1) }
+  rule(:wire_name) { match('[a-z]').repeat(1) }
+end
+
+class InstructionsParser
+  def initialize(instructions_reader)
+    @instructions_reader = instructions_reader
+  end
+  
+  include Enumerable
+  def each
+    while line = @instructions_reader.gets
+      line = line.chomp
+      yield parse_one_instruction(line)
+    end
+  end
+  
+  private
+
+  def parse_one_instruction(line)
+    {} # TODO
   end
 end
 
--- advent-of-code-day-7/test/main_test.rb
+++ advent-of-code-day-7/test/main_test.rb
@@ -1,8 +1,51 @@
 require_relative 'test_helper'
 
-class MainTest < MiniTest::Test
-  def test_hello
-    person = Person.new
-    assert_equal "Hello!", person.greet
+class InstructionsParsingTest < MiniTest::Test
+  def test_parse_no_instructions
+    instructions = ""
+    parsed = InstructionsParser.new(make_reader(instructions))
+    assert_equal [], parsed.to_a
+  end
+  
+  def test_parse_one_instruction
+    instructions = "123 -> x"
+    parsed = InstructionsParser.new(make_reader(instructions))
+    assert_equal [{from: 123, to: :x}], parsed.to_a
+  end
+  
+  def test_parse_one_instruction_with_ending_newline
+    instructions = "123 -> x\n"
+    parsed = InstructionsParser.new(make_reader(instructions))
+    assert_equal [{}], parsed.to_a
+  end
+  
+  def test_parse_all_instruction_types
+    instructions = [
+      "123 -> x",
+      "x AND y -> z",
+      "x OR y -> a",
+      "p LSHIFT 2 -> q",
+      "p RSHIFT 1 -> r",
+      "NOT e -> f",
+    ].join("\n")
+    parsed = InstructionsParser.new(make_reader(instructions))
+    assert_equal [
+      {},
+    ], parsed.to_a
+  end
+  
+  def test_raise_when_parsing_unknown_instruction
+    bad_instruction = "color lights 0,0 through 999,999 red"
+    instructions = bad_instruction
+    parsed = InstructionsParser.new(make_reader(instructions))
+    
+    error = assert_raises(InstructionsParser::ParseError) { parsed.to_a }
+    assert_includes error.message, bad_instruction
+  end
+  
+  private
+  
+  def make_reader(instruction_string)
+    StringIO.new(instruction_string)
   end
 end

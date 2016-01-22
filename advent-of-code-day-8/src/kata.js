d5d30372f9851888d6f0291b0fe7da3ff33311f5
Rory O’Kane <rory@roryokane.com>
2016-01-22 18:06:19 -0500

    Write the testable part of day 8 part 1
    I still need to write the main method that reads from stdin and adds and subtracts character counts.

--- advent-of-code-day-8/src/kata.js
+++ advent-of-code-day-8/src/kata.js
@@ -1,3 +1,51 @@
-function Person() {
-  return {greet: 'Hello!'};
+class StringContentsCharCounter {
+  constructor(stringContents) {
+    this.stringContents = stringContents;
+    this.index = 0;
+    this.charCount = 0;
+  }
+  
+  count() {
+    while (this.indexIsValid()) {
+      this.countAndAdvanceIndex();
+    }
+    return this.charCount;
+  }
+  
+  indexIsValid() {
+    return this.index < this.stringContents.length;
+  }
+  
+  countAndAdvanceIndex() {
+    const nextChar = this.stringContents[this.index];
+    if (nextChar === '\\') {
+      this.index++; // past the backslash
+      this.countAndAdvanceForEscapeSequence();
+    } else {
+      this.index++;
+      this.charCount++;
+    }
+  }
+  
+  countAndAdvanceForEscapeSequence() {
+    const nextChar = this.stringContents[this.index];
+    if (nextChar === 'x') {
+      this.index++; // past 'x'
+      this.countAndAdvanceForEscapedHexDigits();
+    } else {
+      this.index++; // past the escaped character
+      this.charCount++;
+    }
+  }
+  
+  countAndAdvanceForEscapedHexDigits() {
+    this.index += 2; // past the two hex digits
+    this.charCount++;
+  }
+}
+
+function countStringLiteralCharacters(stringLiteral) {
+  const contents = stringLiteral.slice(1, -1); // cut out surrounding quotes
+  const contentsCounter = new StringContentsCharCounter(contents);
+  return contentsCounter.count();
 }
--- advent-of-code-day-8/test/kata_spec.js
+++ advent-of-code-day-8/test/kata_spec.js
@@ -1,6 +1,33 @@
-describe('Person', function() {
-  it('has a greeting', function() {
-    var person = new Person();
-    expect(person.greet).toBe('Hello!');
+describe('countStringLiteralCharacters', function() {
+  // note that backslashes are double-escaped in test strings
+  
+  it('counts characters in a simple string', function() {
+    var string = '"abc"';
+    expect(countStringLiteralCharacters(string)).toBe(3);
+  });
+  
+  it('counts 0 characters in an empty string literal', function() {
+    var string = '""';
+    expect(countStringLiteralCharacters(string)).toBe(0);
+  });
+  
+  it('counts escaped backslashes as one character', function() {
+    var string = '"a\\\\c"';
+    expect(countStringLiteralCharacters(string)).toBe(3);
+  });
+  
+  it('counts escaped double quotes as one character', function() {
+    var string = '"a\\"c"';
+    expect(countStringLiteralCharacters(string)).toBe(3);
+  });
+  
+  it('counts two-digit hexadecimal escapes as one character', function() {
+    var string = '"a\\xd4c"';
+    expect(countStringLiteralCharacters(string)).toBe(3);
+  });
+  
+  it('counts correctly with a combination of escapes', function() {
+    var string = '"\\\\ \\x78\\x9a \\"yes\\" /"';
+    expect(countStringLiteralCharacters(string)).toBe(12);
   });
 });

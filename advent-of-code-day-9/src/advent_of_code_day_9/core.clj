ebb18609d75cd947d287b8be6faa399ec23db2e4
Rory O’Kane <rory@roryokane.com>
2016-01-29 07:17:50 -0500

    Complete day 9 part 1

--- advent-of-code-day-9/src/advent_of_code_day_9/core.clj
+++ advent-of-code-day-9/src/advent_of_code_day_9/core.clj
@@ -1,7 +1,87 @@
 (ns advent-of-code-day-9.core
+  (:require [clojure.string :as string])
   (:gen-class))
 
+
+(defn parse-distance [distance-string]
+  (let [matches (re-matches #"(.+) to (.+) = (\d+)" distance-string)]
+    {:locations [(nth matches 1) (nth matches 2)]
+     :distance (Integer/parseInt (nth matches 3))}))
+
+(defn- add-directed-edge-to-graph [graph [from to] cost]
+  "Returns the graph with a new one-way edge, creating nodes if they do not exist."
+  (if (graph from)
+    (assoc-in graph [from to] cost)
+    (assoc graph from {to cost})))
+
+(defn add-connection-to-graph [graph {[loc1 loc2] :locations, distance :distance}]
+  "Returns the graph with a new two-way connection, creating nodes if they do not exist."
+  (let [all-directed-edges [[loc1 loc2] [loc2 loc1]]]
+    (reduce (fn [graph edge]
+              (add-directed-edge-to-graph graph edge distance))
+            graph all-directed-edges)))
+
+(defn parse-distance-graph [lines]
+  (->> lines
+       (map parse-distance)
+       (reduce add-connection-to-graph {})))
+
+
+;; I might not need these functions after all
+(defn- remove-directed-edge-from-graph [graph [from to]]
+  (update-in graph [from] dissoc to))
+(defn- remove-connection-from-graph [graph [loc1 loc2]]
+  (let [all-directed-edges [[loc1 loc2] [loc2 loc1]]]
+    (reduce remove-directed-edge-from-graph graph all-directed-edges)))
+
+(defn- remove-all-edges-to-node [graph to-node]
+  (reduce (fn [graph from-node]
+            (update-in graph [from-node] dissoc to-node))
+          graph (keys graph)))
+
+(defn remove-node-from-graph [graph node]
+  (-> graph
+      (dissoc node)
+      (remove-all-edges-to-node node)))
+
+(defn- nodes-in-graph [graph]
+  (keys graph))
+
+(declare all-spanning-paths)
+(defn spanning-paths-starting-with-node [graph node]
+  (map (fn [remaining-path]
+         (cons node remaining-path))
+       (all-spanning-paths (remove-node-from-graph graph node))))
+
+(defn all-spanning-paths [graph]
+  ;; uses brute-force recursion
+  "A lazy seq of all spanning paths, assuming all nodes in the graph are connected to all other nodes. Reversed versions of existing paths are still included."
+  (if (empty? (nodes-in-graph graph))
+    '([]) ; not '(), as would otherwise be implied
+    (mapcat (partial spanning-paths-starting-with-node graph)
+          (nodes-in-graph graph))))
+
+(defn- distance-of-edge [graph [loc1 loc2]]
+  ((graph loc1) loc2))
+
+(defn distance-of-path [graph path]
+  (apply + (map (partial distance-of-edge graph)
+                (partition 2 1 path))))
+
+(defn- min-by [f coll]
+  (apply min-key f coll))
+
+(defn find-shortest-path [graph]
+  (min-by (partial distance-of-path graph) (all-spanning-paths graph)))
+
+(defn find-shortest-distance [graph]
+  (distance-of-path graph (find-shortest-path graph)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Read in distances between locations and print the length of the shortest route that visits all locations."
   [& args]
-  (println "Hello, World!"))
+  (->> (slurp *in*)
+       (string/split-lines)
+       (parse-distance-graph)
+       (find-shortest-distance)
+       (println)))
--- advent-of-code-day-9/test/advent_of_code_day_9/core_test.clj
+++ advent-of-code-day-9/test/advent_of_code_day_9/core_test.clj
@@ -2,6 +2,51 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-day-9.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest input-parsing
+  (testing "parsing a line describing a distance"
+    (is (= {:locations ["London" "Dublin"] :distance 464}
+           (parse-distance "London to Dublin = 464"))))
+  
+  (testing "adding one connection to a graph"
+    (is (= {"London" {"Dublin" 464, "Belfast" 518}
+            "Dublin" {"London" 464}}
+           (add-connection-to-graph
+             {"London" {"Belfast" 518}}
+             {:locations ["London" "Dublin"] :distance 464}))))
+  
+  (testing "parsing all input into a graph"
+    (let [lines ["London to Dublin = 464"
+                 "London to Belfast = 518"
+                 "Dublin to Belfast = 141"]]
+      (is (= {"London" {"Dublin" 464, "Belfast" 518}
+              "Belfast" {"London" 518, "Dublin" 141}
+              "Dublin" {"London" 464, "Belfast" 141}}
+             (parse-distance-graph lines))))))
+
+(deftest finding-shortest-distance
+  (let [test-graph
+        {"London" {"Dublin" 464, "Belfast" 518}
+         "Belfast" {"London" 518, "Dublin" 141}
+         "Dublin" {"London" 464, "Belfast" 141}}]
+    
+    (testing "removing a node from a graph"
+      (is (= {"London" {"Dublin" 464}
+              "Dublin" {"London" 464}}
+             (remove-node-from-graph test-graph "Belfast"))))
+    
+    (testing "finding all spanning paths"
+      (is (= #{["Dublin" "London" "Belfast"]
+               ["Dublin" "Belfast" "London"]
+               ["London" "Dublin" "Belfast"]
+               ["London" "Belfast" "Dublin"]
+               ["Belfast" "Dublin" "London"]
+               ["Belfast" "London" "Dublin"]}
+             (set (all-spanning-paths test-graph)))))
+
+    (testing "finding shortest path"
+      (let [shortest-path (find-shortest-path test-graph)]
+        (is (or (= ["London" "Dublin" "Belfast"] shortest-path)
+                (= ["Belfast" "Dublin" "London"] shortest-path)))))
+
+    (testing "finding shortest distance"
+      (is (= 605 (find-shortest-distance test-graph))))))

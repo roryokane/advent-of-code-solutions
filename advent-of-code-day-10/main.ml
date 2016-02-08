d34b6e8ee5aa4c6af3f4996b3297ff600e17aadc
Rory O’Kane <rory@roryokane.com>
2016-02-08 07:39:25 -0500

    Rearrange lines in day 10 to be easier to read

--- advent-of-code-day-10/main.ml
+++ advent-of-code-day-10/main.ml
@@ -7,8 +7,7 @@ let () =
   let require_line = (function
       | None -> failwith "input required"
       | Some line -> line) in
-  In_channel.input_line stdin
-  |> require_line
+  In_channel.input_line stdin |> require_line
   |> MathSequence.iterate ~f:LookAndSay.next ~count:times_to_repeat
   |> String.length |> Int.to_string
   |> print_string
